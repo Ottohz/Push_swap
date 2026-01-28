24d6918de7130ea2f5dee6a4e479f9e83983e2d9
Ottohz <131500626+Ottohz@users.noreply.github.com>
2026-01-28 18:02:01 +0100

    Update stack_utils.c

--- stack_utils.c
+++ stack_utils.c
@@ -3,97 +3,105 @@
 /*                                                        :::      ::::::::   */
 /*   stack_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ovon-hir <ovon-hir@student.s19.be>         +#+  +:+       +#+        */
+/*   By: utente <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/16 15:18:40 by ovon-hir          #+#    #+#             */
-/*   Updated: 2026/01/16 16:42:46 by ovon-hir         ###   ########.fr       */
+/*   Created: 2023/04/02 09:49:59 by utente            #+#    #+#             */
+/*   Updated: 2023/04/03 19:29:16 by utente           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <limits.h>
 
-int     *find_last_node(t_stack_node *head)
+t_stack_node	*find_last_node(t_stack_node *head)
 {
-    if (NULL == head)
-        return (NULL);
-    while (head -> next)
-    {
-        head = head -> next;
-    }
-        return (head);
+	if (NULL == head)
+		return (NULL);
+	while (head->next)
+		head = head->next;
+	return (head);
 }
 
-void    append_node(t_stack_node **stack, int nbr)
+/*
+ * Search for the last node and append 
+ * 🚨 Pay attention if the stack empty?
+*/
+void	append_node(t_stack_node **stack, int nbr)
 {
-    t_stack_node    *node;
-    t_stack_node    *last_node;
+	t_stack_node	*node;
+	t_stack_node	*last_node;
 
-    if (NULL == stack)
-        return ;
-    node = malloc(sizeof(t_stack_node));
-        if (NULL == node)
-            return ;
-    node->next = NULL;
-    node->value = nbr;
-
-    if (NULL == *stack)
-    {
-       *stack = node;
-        node->prev = NULL;
-    }
-    else
-    {
-        last_node = find_last_node(*stack);
-        last_node->next = node;
-        node->prev = last_node;
-    }
+	if (NULL == stack)
+		return ;
+	node = malloc(sizeof(t_stack_node));
+	if (NULL == node)
+		return ;
+	node->next = NULL;
+	node->value = nbr;
+	if (NULL == *stack)
+	{
+		*stack = node;
+		node->prev = NULL;
+	}
+	else
+	{
+		last_node = find_last_node(*stack);
+		last_node->next = node;
+		node->prev = last_node;
+	}
 }
 
-t_stack_node    *find_smallest(t_stack_node *stack)
+/*
+ * Find the smallest value node
+*/
+t_stack_node	*find_smallest(t_stack_node *stack)
 {
-    long    smallest;
-    t_stack_node    *smallest_node;
+	long			smallest;
+	t_stack_node	*smallest_node;
 
-    if (NULL == stack)
-        return ;
-    smallest = LONG_MAX;
-    while (stack)
-    {
-        if (stack->value < smallest)
-        {
-            smallest = stack->value;
-            smallest_node = stack;
-        }
-        stack = stack->next;
-    }
-return (smallest_node);
+	if (NULL == stack)
+		return (NULL);
+	smallest = LONG_MAX;
+	while (stack)
+	{
+		if (stack->value < smallest)
+		{
+			smallest = stack->value;
+			smallest_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (smallest_node);
 }
 
-t_stack_node    *return_cheapest(t_stack_node   *stack)
+/*
+ * Return the cheapest node 
+ * that is already flagged
+*/
+t_stack_node	*return_cheapest(t_stack_node *stack)
 {
-    if (NULL == stack)
-        return (NULL);
-    while (stack)
-    {
-        if (stack->cheapest)
-            return (stack);
-        stack = stack->next;
-    }
-    return (NULL);
+	if (NULL == stack)
+		return (NULL);
+	while (stack)
+	{
+		if (stack->cheapest)
+			return (stack);
+		stack = stack->next;
+	}
+	return (NULL);
 }
 
-int     stack_len(t_stack_node  *stack)
+int	stack_len(t_stack_node *stack)
 {
-    int     count;
+	int	count;
 
-    if (NULL == stack)
-        return (0)
-    count = 0;
-    while(stack)
-    {
-        ++count;
-        stack = stack->next;
-    }
-    return (count);
+	if (NULL == stack)
+		return (0);
+	count = 0;
+	while (stack)
+	{
+		++count;
+		stack = stack->next;
+	}
+	return (count);
 }
