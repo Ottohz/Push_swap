c90394bdd25826efb38ed779a7747ec239400f82
Ottohz <131500626+Ottohz@users.noreply.github.com>
2026-01-28 18:01:43 +0100

    Rename and refactor atoi function for clarity
    Refactor ft_atoi_long to ft_atol and improve readability.

--- stack_init.c
+++ stack_init.c
@@ -1,47 +1,72 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_init.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: utente <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/02 09:42:38 by utente            #+#    #+#             */
+/*   Updated: 2023/04/05 10:54:02 by utente           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 #include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-static long ft_atoi_long(const char *str)
+/*
+ *  atol, i need it to check eventual overflows
+ *  converting every string into a long value
+*/
+static long	ft_atol(const char *str)
 {
-    long    num;
-    int     i;
-    int     neg;
-    num = 0;
-    i = 0;
-    neg = 1;
+	long	num;
+	int		isneg;
+	int		i;
 
-    while (str[i] && (str[i] == " " || str[i] == '\t'
-            || str[i] == '\n'|| str[i] == '\r' 
-            || str[i] == '\v' || str[i] == '\f'))
-        {
-            i++;
-        }
-    if (str[i] == '+')
-        {
-            i++;
-        }
-    if (str[i] == '-')
-        {
-            neg *= -1;
-            i++;
-        }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        num = (num * 10) + (str[i] - '0')
-    }
-        return (num * neg);
+	num = 0;
+	isneg = 1;
+	i = 0;
+	while (str[i] && (str[i] == ' ' || str[i] == '\t'
+			|| str[i] == '\n' || str[i] == '\r'
+			|| str[i] == '\v' || str[i] == '\f'))
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		isneg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = (num * 10) + (str[i] - '0');
+		i++;
+	}
+	return (num * isneg);
 }
 
-    void	stack_init(t_stack_node **a, char **argv, bool flag_argc_2)
-    {
-        long    nbr;
-        int     i;
-   i = 0;
-        while (argv[i])
-        {
-        if (error_syntax(argv[i]))
+/*
+ * Create the stack with the command line values
+ * Checks are embedded in the creation itslef
+ * 		~Duplicate values
+ * 		~Over|Underflow
+ * 		~Syntax errors
+ *
+ * 	🏁 Flag is useful cause if true, i have the argv in the HEAP to free
+ *
+*/
+void	stack_init(t_stack_node **a, char **argv, bool flag_argc_2)
+{
+	long	nbr;
+	int		i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (error_syntax(argv[i]))
 			error_free(a, argv, flag_argc_2);
 		nbr = ft_atol(argv[i]);
 		if (nbr > INT_MAX || nbr < INT_MIN)
@@ -54,4 +79,3 @@ static long ft_atoi_long(const char *str)
 	if (flag_argc_2)
 		free_matrix(argv);
 }
-     
